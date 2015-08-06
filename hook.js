4bf17e169d3adaaf96fa7a23b81e8b190c7e1244
Party Hard <admin@partyhard-app.com>
2015-08-06 16:09:27 -0400

    changes in hook.js

--- hook.js
+++ hook.js
@@ -9,7 +9,7 @@ github.listen();
 
 github.on('partyhard-www:refs/heads/master', function (event, data) {
   console.log('Change on master');
-  execFile('./hook.sh', function(error, stdout, stderr) {
+  execFile('sh hook.sh', function(error, stdout, stderr) {
     if (error) console.error(error);
     console.log('Hook.sh completed');
   });
