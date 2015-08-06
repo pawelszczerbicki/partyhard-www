0a840eb34dc6c1ff648189e524ee126b3ea94b76
shark <szczerbicki.pawel@gmail.com>
2015-08-06 19:12:08 +0200

    add hook

---
+++ hook.js
@@ -0,0 +1,17 @@
+var execFile = require('child_process').execFile;
+var githubhook = require('githubhook');
+var github = githubhook({
+  port: 9002,
+  path: '/'
+});
+
+github.listen();
+
+github.on('partyhard-www:refs/heads/master', function (event, data) {
+  console.log('Change on master');
+  execFile('./hook.sh', function(error, stdout, stderr) {
+    if (error) console.error(error);
+    console.log('Hook.sh completed');
+  });
+});
+
